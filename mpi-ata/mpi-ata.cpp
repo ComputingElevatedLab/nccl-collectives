b1958cba105dee1d547f05631abd5f9fe83e49a7
Andres Sewell <sewellandres@gmail.com>
2023-01-25 19:06:35 -0700

    Vary buffer sizes for benchmarking

--- mpi-ata/mpi-ata.cpp
+++ mpi-ata/mpi-ata.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 #include <mpi.h>
 
@@ -18,42 +19,86 @@ int main(int argc, char** argv) {
   MPICHECK(MPI_Comm_size(MPI_COMM_WORLD, &size));
   MPICHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
 
-  // Send and recieve buffers must be the same size
-  int send_data[size];
-  int recv_data[size];
+  int* send_data;
+  int* recv_data;
 
-  // Fill the send buffer with each process rank
-  for (int i = 0; i < size; i++) {
-    send_data[i] = rank;
+  // Benchmark loop
+  const int num_executions = 100;
+  for (int i = 1; i <= 1000000; i *= 10) {
+    // Send and recieve buffers must be the same size
+    int multiplier = 1 * i;
+    const int buffer_size = size * multiplier;
+    send_data = new int[buffer_size];
+    recv_data = new int[buffer_size];
+
+    // Fill the send buffer with each process rank
+    for (int i = 0; i < buffer_size; i++) {
+      send_data[i] = rank;
+    }
+
+    // Warm-up loop
+    for (int j = 0; j < 5; j++) {
+      for (int j = 0; j < buffer_size; j++) {
+        send_data[j] = rank;
+        recv_data[j] = 0;
+      }
+      MPICHECK(MPI_Barrier(MPI_COMM_WORLD));
+      MPICHECK(MPI_Alltoall(send_data, multiplier, MPI_INT, recv_data, multiplier, MPI_INT, MPI_COMM_WORLD));
+    }
+
+    std::vector<float> times(num_executions);
+    for (int j = 0; j < num_executions; j++) {
+      // Reset buffers
+      for (int k = 0; k < buffer_size; k++) {
+        send_data[k] = rank;
+        recv_data[k] = 0;
+      }
+      MPICHECK(MPI_Barrier(MPI_COMM_WORLD));
+
+      // Perform all to all
+      auto start = std::chrono::high_resolution_clock::now();
+      MPICHECK(MPI_Alltoall(send_data, multiplier, MPI_INT, recv_data, multiplier, MPI_INT, MPI_COMM_WORLD));
+      auto stop = std::chrono::high_resolution_clock::now();
+
+      // Compute elapsed time
+      auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+      const float localElapsedTime = duration.count();
+
+      MPI_Barrier(MPI_COMM_WORLD);
+      float elapsedTime;
+      MPI_Reduce(&localElapsedTime, &elapsedTime, 1, MPI_FLOAT, MPI_MAX, 0, MPI_COMM_WORLD);
+      if (rank == 0) {
+        times[j] = localElapsedTime;
+      }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (rank == 0) {
+      float sum = 0;
+      for (int i = 0; i < num_executions; i++) {
+        sum += times[i];
+      }
+      float average = sum / num_executions;
+
+      std::ofstream log;
+      log.open ("run.log", std::ios_base::app);
+      log << "mpi-ata w/ " << buffer_size * sizeof(int)
+          << " byte buffer: " << average << " ms" << std::endl;
+      log.close();
+    }
+
+    // Verify that all ranks have the same thing in their recieve buffer
+    std::cout << "Rank " << rank << " received data: [";
+    for (int i = 0; i < buffer_size; i++) {
+      std::cout << " " << recv_data[i] << " ";
+    }
+    std::cout << "]" << std::endl;
+
+    // Free allocated memory
+    delete[] send_data;
+    delete[] recv_data;
   }
-
-  // Use MPI_Alltoall to send and receive each rank
-  auto start = std::chrono::high_resolution_clock::now();
-  MPICHECK(MPI_Alltoall(send_data, 1, MPI_INT, recv_data, 1, MPI_INT, MPI_COMM_WORLD));
-  auto stop = std::chrono::high_resolution_clock::now();
-
-  // Compute elapsed time
-  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-  const float localElapsedTime = duration.count();
-  // std::cout << "Rank " << rank << " elapsed all-to-all time: " << localElapsedTime << " ms" << std::endl;
-
-  // Verify that all ranks have the same thing in their recieve buffer
-  std::cout << "Rank " << rank << " received data: [";
-  for (int i = 0; i < size; i++) {
-    std::cout << " " << recv_data[i] << " ";
-  }
-  std::cout << "]" << std::endl;
-
-  MPI_Barrier(MPI_COMM_WORLD);
-  float elapsedTime;
-  MPI_Reduce(&localElapsedTime, &elapsedTime, 1, MPI_FLOAT, MPI_MAX, 0, MPI_COMM_WORLD);
-  if (rank == 0) {
-    std::ofstream log;
-    log.open ("run.log", std::ios_base::app);
-    log << "mpi-ata: " << elapsedTime << " ms" << std::endl;
-    log.close();
-  }
-
+  
   // Finalize MPI
   MPICHECK(MPI_Finalize());
   return 0;
--- nccl-ata-bruck/nccl-ata-bruck.cu
+++ nccl-ata-bruck/nccl-ata-bruck.cu
@@ -1,148 +1,157 @@
-// Source: https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/examples.html#communicator-creation-and-destruction-examples
+// Source: https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/examples.html
 #include <fstream>
 #include <iostream>
+#include <numeric>
+#include <vector>
 
 #include <mpi.h>
 #include <nccl.h>
 
-#include "../common/error-catch.cpp"
 #include "../common/bruck.cu"
+#include "../common/error-catch.cpp"
 #include "../common/error-catch.cu"
 #include "../common/hostname.cu"
 
-int main(int argc, char* argv[])
-{
-    // Initialize MPI
-    MPICHECK(MPI_Init(&argc, &argv));
+int main(int argc, char *argv[]) {
+  // Initialize MPI
+  MPICHECK(MPI_Init(&argc, &argv));
 
-    // Set MPI size and rank
-    int size;
-    int rank;
-    MPICHECK(MPI_Comm_size(MPI_COMM_WORLD, &size));
-    MPICHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
-    
-    int count;
-    CUDACHECK(cudaGetDeviceCount(&count));
-    if (rank == 0) {
-        std::cout << "nccl-ata-bruck" << std::endl;
-        std::cout << "CUDA devices available: " << count << std::endl; 
+  // Set MPI size and rank
+  int size;
+  int rank;
+  MPICHECK(MPI_Comm_size(MPI_COMM_WORLD, &size));
+  MPICHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+
+  int count;
+  CUDACHECK(cudaGetDeviceCount(&count));
+  if (rank == 0) {
+    std::cout << "nccl-ata-bruck" << std::endl;
+    std::cout << "CUDA devices available: " << count << std::endl;
+  }
+
+  // Figure out what host the current MPI process is running on
+  uint64_t hostHashs[size];
+  char hostname[1024];
+  getHostName(hostname, 1024);
+  hostHashs[rank] = getHostHash(hostname);
+  MPICHECK(MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, hostHashs, sizeof(uint64_t), MPI_BYTE, MPI_COMM_WORLD));
+
+  // Compute and set the local rank based on the hostname
+  int local_rank = 0;
+  for (int i = 0; i < size; i++) {
+    if (i == rank) {
+      break;
     }
-
-    // Figure out what host the current MPI process is running on
-    uint64_t hostHashs[size];
-    char hostname[1024];
-    getHostName(hostname, 1024);
-    hostHashs[rank] = getHostHash(hostname);
-    MPICHECK(MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, hostHashs, sizeof(uint64_t), MPI_BYTE, MPI_COMM_WORLD));
-
-    // Compute and set the local rank based on the hostname
-    int local_rank = 0;
-    for (int i = 0; i < size; i++) {
-        if (i == rank) {
-            break;
-        }
-        if (hostHashs[i] == hostHashs[rank]) {
-            local_rank++;
-        }
+    if (hostHashs[i] == hostHashs[rank]) {
+      local_rank++;
     }
-    std::cout << "Rank " << rank << ": on host " << hostname << " using GPU " << local_rank << std::endl;
+  }
 
-    // Initialize a unique NCCL ID at process 0 and broadcast it to all others
-    ncclUniqueId id;
-    if (rank == 0) {
-        ncclGetUniqueId(&id);
-    }
-    MPICHECK(MPI_Bcast((void*) &id, sizeof(id), MPI_BYTE, 0, MPI_COMM_WORLD));
+  // Initialize a unique NCCL ID at process 0 and broadcast it to all others
+  ncclUniqueId id;
+  if (rank == 0) {
+    ncclGetUniqueId(&id);
+  }
+  MPICHECK(MPI_Bcast((void*) &id, sizeof(id), MPI_BYTE, 0, MPI_COMM_WORLD));
 
-    // Allocate memory for host variables
-    const int bytes = size * sizeof(int);
-    int* h_send_data = new int[size];
-    int* h_recv_data = new int[size];
+  // Initialize NCCL
+  ncclComm_t comm;
+  cudaStream_t stream;
+  CUDACHECK(cudaSetDevice(local_rank));
+  NCCLCHECK(ncclCommInitRank(&comm, size, id, rank));
+  CUDACHECK(cudaStreamCreate(&stream));
 
-    // Fill the send buffer with each process rank
-    for (int i = 0; i < size; i++) {
-        h_send_data[i] = rank;
-    }
+  // Host variables
+  int *h_send_data;
+  int *h_recv_data;
 
-    // Allocate memory for device variables
-    cudaStream_t stream;
-    int* d_send_data = nullptr;
-    int* d_recv_data = nullptr;
-    CUDACHECK(cudaSetDevice(local_rank));
+  // Device variables
+  int *d_send_data;
+  int *d_recv_data;
+
+  // Benchmark loop
+  const int num_executions = 100;
+  for (int i = 1; i <= 1000000; i *= 10) {
+    // Send and recieve buffers must be the same size
+    int multiplier = 1 * i;
+    const int buffer_size = size * multiplier;
+    const int bytes = buffer_size * sizeof(int);
+
+    h_send_data = new int[buffer_size];
+    h_recv_data = new int[buffer_size];
     CUDACHECK(cudaMalloc((void**) &d_send_data, bytes));
     CUDACHECK(cudaMalloc((void**) &d_recv_data, bytes));
-    CUDACHECK(cudaMemcpy(d_send_data, h_send_data, bytes, cudaMemcpyDefault));
-    CUDACHECK(cudaStreamCreate(&stream));
 
-    // Initialize NCCL
-    ncclComm_t comm;
-    NCCLCHECK(ncclCommInitRank(&comm, size, id, rank));
+    // Fill the send buffer with each process rank
+    for (int i = 0; i < buffer_size; i++) {
+      h_send_data[i] = rank;
+    }
+    
+    CUDACHECK(cudaMemcpy(d_send_data, h_send_data, bytes, cudaMemcpyDefault));
+    CUDACHECK(cudaMemset(d_recv_data, 0, bytes));
 
     // Warm-up loop
-    for (int i = 0; i < 5; i++) {
-        CUDACHECK(cudaMemcpy(d_send_data, h_send_data, bytes, cudaMemcpyDefault));
-        CUDACHECK(cudaMemset(d_recv_data, 0, bytes));
-        MPICHECK(MPI_Barrier(MPI_COMM_WORLD));
-        ncclBruck(2, (char*) d_send_data, 1, ncclInt, (char*) d_recv_data, 1, ncclInt, comm, stream);
+    for (int j = 0; j < 5; j++) {
+      CUDACHECK(cudaMemcpy(d_send_data, h_send_data, bytes, cudaMemcpyDefault));
+      CUDACHECK(cudaMemset(d_recv_data, 0, bytes));
+      MPICHECK(MPI_Barrier(MPI_COMM_WORLD));
+      ncclBruck(2, (char*) d_send_data, multiplier, ncclInt, (char*) d_recv_data, multiplier, ncclInt, comm, stream);
     }
 
-    // Benchmark loop
-    const int num_executions = 100;
     std::vector<float> times(num_executions);
-    for (int i = 0; i < num_executions; i++) {
-        // Reset send data
-        CUDACHECK(cudaMemcpy(d_send_data, h_send_data, bytes, cudaMemcpyDefault));
-        CUDACHECK(cudaMemset(d_recv_data, 0, bytes));
-        MPICHECK(MPI_Barrier(MPI_COMM_WORLD));
+    for (int j = 0; j < num_executions; j++) {
+      // Reset buffers
+      CUDACHECK(cudaMemcpy(d_send_data, h_send_data, bytes, cudaMemcpyDefault));
+      CUDACHECK(cudaMemset(d_recv_data, 0, bytes));
+      MPICHECK(MPI_Barrier(MPI_COMM_WORLD));
 
-        // Setup a new timer
-        cudaEvent_t start, stop;
-        cudaEventCreate(&start);
-        cudaEventCreate(&stop);
+      // Create CUDA events
+      cudaEvent_t start, stop;
+      cudaEventCreate(&start);
+      cudaEventCreate(&stop);
 
-        // Perform all-to-all to send and receive
-        cudaEventRecord(start, 0);
-        ncclBruck(2, (char*) d_send_data, 1, ncclInt, (char*) d_recv_data, 1, ncclInt, comm, stream);
-        cudaEventRecord(stop, 0);
-        cudaEventSynchronize(stop);
+      // Perform all-to-all
+      cudaEventRecord(start, 0);
+      ncclBruck(2, (char*) d_send_data, multiplier, ncclInt, (char*) d_recv_data, multiplier, ncclInt, comm, stream);
+      cudaEventRecord(stop, 0);
+      cudaEventSynchronize(stop);
 
-        // Compute elapsed time
-        float localElapsedTime;
-        cudaEventElapsedTime(&localElapsedTime, start, stop);
+      // Compute elapsed time
+      float localElapsedTime;
+      cudaEventElapsedTime(&localElapsedTime, start, stop);
 
-        // Destroy CUDA events
-        cudaEventDestroy(start);
-        cudaEventDestroy(stop);
+      // Destroy CUDA events
+      cudaEventDestroy(start);
+      cudaEventDestroy(stop);
 
-        // Reduce and save results
-        MPICHECK(MPI_Barrier(MPI_COMM_WORLD));
-        float elapsedTime;
-        MPICHECK(MPI_Reduce(&localElapsedTime, &elapsedTime, 1, MPI_FLOAT, MPI_MAX, 0, MPI_COMM_WORLD));
-        if (rank == 0){
-            times[i] = localElapsedTime;
-        }
+      MPICHECK(MPI_Barrier(MPI_COMM_WORLD));
+      float elapsedTime;
+      MPICHECK(MPI_Reduce(&localElapsedTime, &elapsedTime, 1, MPI_FLOAT, MPI_MAX, 0, MPI_COMM_WORLD));
+      if (rank == 0) {
+        times[j] = localElapsedTime;
+      }
     }
-
-    // Verify that all processes have the same thing in their recieve buffer
-    CUDACHECK(cudaMemcpy(h_recv_data, d_recv_data, bytes, cudaMemcpyDefault));
-    std::cout << "Rank " << rank << ": received data: [";
-    for (int i = 0; i < size; i++) {
-        std::cout << " " << h_recv_data[i] << " ";
-    }
-    std::cout << "]" << std::endl;
 
     if (rank == 0) {
-        float sum = 0;
-        for (int i = 0; i < num_executions; i++) {
-            sum += times[i];
-        }
-        float average = sum / num_executions;
-        
-        std::ofstream log;
-        log.open ("run.log", std::ios_base::app);
-        log << "nccl-ata-bruck | " << num_executions << " executions | " << bytes << " bytes:" << average << " ms" << std::endl;
-        log.close();
+      float sum = 0;
+      for (int i = 0; i < num_executions; i++) {
+        sum += times[i];
+      }
+      float average = sum / num_executions;
+
+      std::ofstream log;
+      log.open("run.log", std::ios_base::app);
+      log << "nccl-ata-bruck w/ " << bytes << " byte buffer: " << average << " ms" << std::endl;
+      log.close();
     }
+
+    // Verify that all ranks have the same thing in their recieve buffer
+    CUDACHECK(cudaMemcpy(h_recv_data, d_recv_data, bytes, cudaMemcpyDefault));
+    std::cout << "Rank " << rank << " received data: [";
+    for (int i = 0; i < size; i++) {
+      std::cout << " " << h_recv_data[i] << " ";
+    }
+    std::cout << "]" << std::endl;
 
     // Free all host variables
     delete[] h_send_data;
@@ -151,11 +160,12 @@ int main(int argc, char* argv[])
     // Free all device variables
     CUDACHECK(cudaFree(d_send_data));
     CUDACHECK(cudaFree(d_recv_data));
+  }
 
-    // Destroy NCCL communicator
-    ncclCommDestroy(comm);
+  // Destroy NCCL communicator
+  ncclCommDestroy(comm);
 
-    // Finalize MPI
-    MPICHECK(MPI_Finalize());
-    return 0;
+  // Finalize MPI
+  MPICHECK(MPI_Finalize());
+  return 0;
 }
